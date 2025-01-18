15603aa0d6518aaa417739a91eecca4d5d349d37
Sajithjay99 <sajithjay99@gmail.com>
2025-01-19 00:18:59 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <ProductCard name = "Audio setup" price ="3450" description="sajith jayasooriya" photoUrl="https://img.drz.lazcdn.com/g/kf/Sacb9b9e8a234469d872b2d8c86070549E.jpg_720x720q80.jpg_.webp"/>
+     
+
+    
+    
+    </>
+  )
+  
+}
+
+export default App
