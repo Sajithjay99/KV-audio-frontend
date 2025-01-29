53e83bd59a43465b76cb45529d0d7988b446631c
Sajithjay99 <sajithjay99@gmail.com>
2025-01-30 01:01:28 +0530

    Add Tailwind CSS configuration and update components for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
+import React from 'react'
 import './App.css'
-import ProductCard from './components/productCard'
+ 
 
 function App() {
-  
-
   return (
-    <>
-    
-    <ProductCard name = "Audio setup" price ="3450" description="sajith jayasooriya" photoUrl="https://img.drz.lazcdn.com/g/kf/Sacb9b9e8a234469d872b2d8c86070549E.jpg_720x720q80.jpg_.webp"/>
-     
+    <div className='w-full h-screen '>
 
-    
-    
-    </>
+
+
+     time 1:17:00
+     
+      </div>
+      
+   
   )
-  
 }
 
 export default App
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,18 +1,14 @@
 import "./productCard.css";
 
 
-export default function ProductCard(props){
+export default function ProductCard(){
 
 
-    console.log( props);
+ 
 
     return (
         <div>
-         <img src={props.photoUrl} />   
-         <span>{props.name}</span>
-         <span>LKR. {props.price}</span>
-
-         <p>{props.description}</p>
+         
         </div>
     )
 
