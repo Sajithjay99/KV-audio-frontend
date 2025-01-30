4209f29efb9406927ecfe60df5d5ed1b32ed3365
Sajithjay99 <sajithjay99@gmail.com>
2025-01-30 13:02:03 +0530

    Add react-icons dependency and update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
 import React from 'react'
 import './App.css'
+
+import { MdDashboard } from "react-icons/md";
+import { FaBookmark } from "react-icons/fa6";
+import { BsFillSpeakerFill } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+
  
 
 function App() {
   return (
-    <div className='w-full h-screen '>
+    <div className='flex w-full h-screen'>
+      
+      <div className='w-[350px] h-full bg-green-200' >
 
+      <button className='w-full h-[40px] text-xl font-semibold  flex justify-center items-center'>
+      <MdDashboard />
+        Dashboard
+      </button>
 
+      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
+      <FaBookmark />
+      Bookings
+      </button>
 
-     time 1:17:00
-     
+      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
+      <BsFillSpeakerFill />
+      Items
+      </button>
+
+      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
+      <FaUserCircle />
+        Users
+      </button>
+
+      
+
+      
+
+        
       </div>
+
+      
+     
+     <div className='w-full h-full bg-red-900'>
+
+
+     
+     </div>
+      </div>
+      
       
    
   )
