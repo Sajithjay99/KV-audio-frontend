17454702379d6c59fc389969815eafe2c330baec
Sajithjay99 <sajithjay99@gmail.com>
2025-01-30 23:38:20 +0530

    Refactor App component and create AdminPage for improved layout and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,19 @@
-import React from 'react'
-import './App.css'
-
-import { MdDashboard } from "react-icons/md";
-import { FaBookmark } from "react-icons/fa6";
-import { BsFillSpeakerFill } from "react-icons/bs";
-import { FaUserCircle } from "react-icons/fa";
-
- 
+import React from "react";
+import "./App.css";
+import AdminPage from "./components/AdminPage";
 
 function App() {
   return (
-    <div className='flex w-full h-screen'>
-      
-      <div className='w-[350px] h-full bg-green-200' >
-
-      <button className='w-full h-[40px] text-xl font-semibold  flex justify-center items-center'>
-      <MdDashboard />
-        Dashboard
-      </button>
-
-      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
-      <FaBookmark />
-      Bookings
-      </button>
-
-      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
-      <BsFillSpeakerFill />
-      Items
-      </button>
-
-      <button className='w-full h-[40px] text-xl font-semibold flex justify-center items-center'>
-      <FaUserCircle />
-        Users
-      </button>
-
-      
-
-      
-
-        
-      </div>
-
-      
-     
-     <div className='w-full h-full bg-red-900'>
 
 
-     
-     </div>
-      </div>
-      
-      
-   
-  )
+    <>
+
+      <AdminPage />
+    
+    
+    </>
+    
+  ); 
 }
 
-export default App
+export default App;
---
+++ src/components/AdminPage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { MdDashboard } from "react-icons/md";
+import { FaBookmark } from "react-icons/fa6";
+import { BsFillSpeakerFill } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function AdminPage() {
+  return (
+    <div className='flex w-full h-screen'>
+      <div className="w-[350px] h-full bg-green-200">
+        <button className="w-full h-[40px] text-xl font-semibold  flex justify-center items-center">
+          <MdDashboard />
+          Dashboard
+        </button>
+
+        <button className="w-full h-[40px] text-xl font-semibold flex justify-center items-center">
+          <FaBookmark />
+          Bookings
+        </button>
+
+        <button className="w-full h-[40px] text-xl font-semibold flex justify-center items-center">
+          <BsFillSpeakerFill />
+          Items
+        </button>
+
+        <button className="w-full h-[40px] text-xl font-semibold flex justify-center items-center">
+          <FaUserCircle />
+          Users
+        </button>
+      </div>
+      
+
+      <div className="flex-1 h-full bg-red-900">
+      
+     
+      </div>
+    </div>
+  );
+}
